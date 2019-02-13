87217fcc2eadd1d199109029eb88521fb2720b02
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-02-13 15:34:53 -0800

    got the game working correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,58 @@
 
-// var answer = "g" ;
-// var guess =  ;
-// var numWins = 0;
-// var numLose = 0;
-
-// for (i=0; i<26;i++){
-//     guess =  ;
-//     if( answer == guess){
-//         alert("You win!")
-//         break;
-//     } else{
-
-//     }
-// }
-
 function setup(){
 
 //insert alphabet variables for computer to choose from
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
     'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
+    //insert array for guesses to be pushed to
+var letterGuessed=[];
+
 // Variables for tracking  wins, losses and ties. They begin at 0.
 var wins = 0;
 var losses = 0;
 var guesses = 10;
 
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+//make random letter selection
+function random(){
+
+return alphabet[Math.floor(Math.random() * alphabet.length)];
+};
+
+var computerChoice = random();
 console.log(computerChoice)
 
-
 document.onkeypress = function(event) {
-    var userGuess = event.key;
 
+    var userGuess = event.key;
+    letterGuessed.push(userGuess);
     if(userGuess === computerChoice){
         wins++;
+        computerChoice = random();
         restart();
-    }else{
+    } else{ 
         guesses--;
     }
 
     if(guesses === 0){
         losses++;
+        computerChoice = random();
         restart(); 
         
     }
 
+    //restarts the game
     function restart() {
         guesses = 10;
         letterGuessed= [];
-        var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+        random();
         console.log(computerChoice);
     }
-
+    //adds relevent info to DOM
     document.getElementById('wins').innerHTML = wins;
     document.getElementById('losses').innerHTML = losses;
     document.getElementById('guesses').innerHTML = guesses;
+    document.getElementById('guessed').innerHTML = letterGuessed;
 
 } 
 };
-
-// restarts the game
-
